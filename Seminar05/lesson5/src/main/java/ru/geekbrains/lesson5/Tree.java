b73361df22bbe8c40e5101677a6226b086a1bb00
Valerii_Panov <val250640@mail.ru>
2023-09-13 10:33:39 +1000

    addFilesInTree

--- Seminar05/lesson5/src/main/java/ru/geekbrains/lesson5/Tree.java
+++ Seminar05/lesson5/src/main/java/ru/geekbrains/lesson5/Tree.java
@@ -6,45 +6,60 @@ public class Tree {
 
     public static void main(String[] args) {
 
-        print(new File("."), "", true);
+        print(new File("."), "", true, true);
 
     }
 
     /**
      * TODO: Доработать метод print, необходимо распечатывать директории и файлы
+     *
      * @param file
      * @param indent
      * @param isLast
      */
-    public static void print(File file, String indent, boolean isLast){
-        System.out.print(indent);
-        if (isLast){
-            System.out.print("└─");
-            indent += "  ";
+    public static void print(File file, String indent, boolean isLast, boolean isFirst) {
+        if (isFirst) {
+            String[] pathDirs = file.getAbsolutePath()
+                    .replace("\\", "/").split("/");
+            System.out.println("\u001B[31m" + pathDirs[pathDirs.length-2] + "\u001B[0m");
+        } else {
+            System.out.print(indent);
+            if (isLast) {
+                System.out.print("└─");
+                indent += "  ";
+            } else {
+                System.out.print("├─");
+                indent += "│ ";
+            }
+            if (file.isDirectory())
+                System.out.println("\u001B[32m" + file.getName() + "\u001B[0m");
+            else
+                System.out.println("\u001B[34m" + file.getName() + "\u001B[0m");
         }
-        else{
-            System.out.print("├─");
-            indent += "│ ";
-        }
-        System.out.println(file.getName());
 
         File[] files = file.listFiles();
         if (files == null)
             return;
 
-        int subDirTotal = 0;
-        for (int i = 0; i < files.length; i++){
-            if (files[i].isDirectory())
-                subDirTotal++;
+        for (int i = 0; i < files.length - 1; i++) {
+            print(files[i], indent, false, false);
         }
+        if (files.length > 0)
+            print(files[files.length - 1], indent, true, false);
 
-        int subDirCounter = 0;
-        for (int i = 0; i < files.length; i++){
-            if (files[i].isDirectory()){
-                subDirCounter++;
-                print(files[i], indent, subDirCounter == subDirTotal);
-            }
-        }
+//        int subTotal = 0;
+//        for (int i = 0; i < files.length; i++) {
+//            if (files[i].isDirectory())
+//                subTotal++;
+//        }
+//
+//        int subDirCounter = 0;
+//        for (int i = 0; i < files.length; i++) {
+//            if (files[i].isDirectory()) {
+//                subDirCounter++;
+//                print(files[i], indent, subDirCounter == subTotal, false);
+//            }
+//        }
 
     }
 
